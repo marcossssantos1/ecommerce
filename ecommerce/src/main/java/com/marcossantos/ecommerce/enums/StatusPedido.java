322df01fee707b3705eb768945ca43d70b2f71c1
marcossssantos1 <marcossantosmsms604@gmail.com>
2026-01-29 15:29:13 -0300

    alterando o enum statuspedido

--- ecommerce/src/main/java/com/marcossantos/ecommerce/enums/StatusPedido.java
+++ ecommerce/src/main/java/com/marcossantos/ecommerce/enums/StatusPedido.java
@@ -2,6 +2,17 @@ package com.marcossantos.ecommerce.enums;
 
 public enum StatusPedido {
 
-	PENDENTE, CONFIRMADO, CANCELADO
+	PENDENTE("Pendente"), CONFIRMADO("Confirmado"), EM_SEPARACAO("Em Separação"), ENVIADO("Enviado"),
+	ENTREGUE("Entregue"), CANCELADO("Cancelado");
+
+	private final String descricao;
+
+	StatusPedido(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 
 }

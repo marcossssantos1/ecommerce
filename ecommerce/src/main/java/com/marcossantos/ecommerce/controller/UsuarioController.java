1bc76f30387986ccc529f995c071745c41055593
marcossssantos1 <marcossantosmsms604@gmail.com>
2026-02-02 20:24:26 -0300

    criando os endpoints de listartodosusuarios e listarporid

--- ecommerce/src/main/java/com/marcossantos/ecommerce/controller/UsuarioController.java
+++ ecommerce/src/main/java/com/marcossantos/ecommerce/controller/UsuarioController.java
@@ -1,9 +1,12 @@
 package com.marcossantos.ecommerce.controller;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,29 +18,36 @@ import com.marcossantos.ecommerce.dto.UsuarioResponse;
 import com.marcossantos.ecommerce.entity.Usuario;
 import com.marcossantos.ecommerce.service.UsuarioService;
 
-
 import jakarta.validation.Valid;
-
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService service;
-	
+
 	@PostMapping
-	public ResponseEntity<UsuarioResponse> criarUsuario(@Valid @RequestBody UsuarioCreateRequest request){
-		
+	public ResponseEntity<UsuarioResponse> criarUsuario(@Valid @RequestBody UsuarioCreateRequest request) {
+
 		Usuario usuario = service.criar(request);
-		
-		URI location = ServletUriComponentsBuilder
-				.fromCurrentRequest()
-				.path("/{id}")
-				.buildAndExpand(usuario.getId())
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(usuario.getId())
 				.toUri();
-		
+
 		return ResponseEntity.created(location).body(usuario.toResponse());
+	}
+
+	@GetMapping
+	public ResponseEntity<List<UsuarioResponse>> listarTodosUsuarios() {
+		service.listarTodosUsuarios();
+		return ResponseEntity.ok().build();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<UsuarioResponse> listarUsuarioPorId(@PathVariable Long id) {
+		Usuario usuario = service.listarUsuarioPorEmail(id);
+		return ResponseEntity.ok(usuario.toResponse());
 	}
 
 }
--- ecommerce/src/main/java/com/marcossantos/ecommerce/service/UsuarioService.java
+++ ecommerce/src/main/java/com/marcossantos/ecommerce/service/UsuarioService.java
@@ -1,5 +1,7 @@
 package com.marcossantos.ecommerce.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,14 @@ public class UsuarioService {
 		Usuario usuario = Usuario.from(request);
 
 		return repository.save(usuario);
+	}
+
+	public List<Usuario> listarTodosUsuarios() {
+		return repository.findAll();
+	}
+
+	public Usuario listarUsuarioPorEmail(Long id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("Usuario não encontrado!!!"));
 	}
 
 }
